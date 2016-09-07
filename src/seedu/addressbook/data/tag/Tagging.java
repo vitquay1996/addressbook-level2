28b47a04918a161548db2386d404394f7a8c16c7
vitquay1996 <vitquay1996@gmail.com>
2016-09-08 00:27:49 +0800

    add Tagging.add

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,11 +1,13 @@
 package seedu.addressbook.data.tag;
 
 import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.tag.UniqueTagList.DuplicateTagException;
 import seedu.addressbook.data.tag.UniqueTagList.TagNotFoundException;
 import seedu.addressbook.ui.*;
 
 public class Tagging {
     private TextUi ui;
+    
     public void removePersonTag(Person target, Tag toRemove) {
         UniqueTagList personTagList = target.getTags();
         try {
@@ -15,5 +17,15 @@ public class Tagging {
         }
         target.setTags(personTagList);
     }
+    
+    public void addPersonTag(Person target, Tag toAdd) {
+        UniqueTagList personTagList = target.getTags();
+        try {
+            personTagList.add(toAdd);
+        } catch (DuplicateTagException e) {
+            ui.showToUser("Already have that tag bro");
+        }
+    }
+    
     
 }
