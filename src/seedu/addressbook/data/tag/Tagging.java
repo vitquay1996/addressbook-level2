0cfb4d226fdddfb56271c915b243f7fb68ee1b29
vitquay1996 <vitquay1996@gmail.com>
2016-09-08 00:33:05 +0800

    add Tagging.add

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.data.tag;
 
+import java.util.ArrayList;
+
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.tag.UniqueTagList.DuplicateTagException;
 import seedu.addressbook.data.tag.UniqueTagList.TagNotFoundException;
@@ -7,6 +9,7 @@ import seedu.addressbook.ui.*;
 
 public class Tagging {
     private TextUi ui;
+    ArrayList<String> tagHistory = new ArrayList<String>(); 
     
     public void removePersonTag(Person target, Tag toRemove) {
         UniqueTagList personTagList = target.getTags();
@@ -16,6 +19,7 @@ public class Tagging {
             ui.showToUser("Cannot find tag");
         }
         target.setTags(personTagList);
+        tagHistory.add("- " + target.getName().toString() + toRemove.tagName);
     }
     
     public void addPersonTag(Person target, Tag toAdd) {
@@ -25,7 +29,13 @@ public class Tagging {
         } catch (DuplicateTagException e) {
             ui.showToUser("Already have that tag bro");
         }
+        target.setTags(personTagList);
+        tagHistory.add("+ " + target.getName().toString() + toAdd.tagName);
     }
     
-    
+    public void showTagHistory() {
+        for (String entry:tagHistory) {
+            ui.showToUser(entry);
+        }
+    }
 }
